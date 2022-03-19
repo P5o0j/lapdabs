99e3f75d504e6f7ea1d74647f233f9a1b686a431
Gerard Press <killerabs@gmail.com>
2022-03-19 11:32:46 +0000

    additional parts of new customer details form

--- new_customer.py
+++ new_customer.py
@@ -4,12 +4,11 @@ Function will be used to add new customer to database
 
 import sqlite3
 import os
-import datetime
 from sqlite3.dbapi2 import Cursor, Row, converters
 import sys
 from time import sleep
 from clear import clear
-import datetime
+from datetime import date
 
 def check_db(filename):
     return os.path.exists(filename)
@@ -24,4 +23,58 @@ cursor = conn.cursor()
 #sleep(2)
 clear()
 
+
 def customer():
+
+    #bulid customer code
+    dataset=cursor.execute("SELECT COUNT(*) FROM customer")
+    count = dataset.fetchall()
+    cuscde = "C" + str(count[0][0])
+
+    #get date when entry was added
+    dateAdd = date.today()
+
+    #get regnum
+
+    #get name
+    while True:
+        name = input('Owner name: ')
+        if name:
+            break
+        else:
+            print('Mandatory field')
+
+    #get surname
+    while True:
+        surname = input('Owner Surname: ')
+        if surname:
+            break
+        else:
+            print('Mandatory field')
+
+    ##get company name
+    while True:
+        company = input('Company name: ')
+        if company == '':
+            break
+        break
+    #get address
+
+    #get phone number
+    while True:
+        phonum = str(input('Phone number: '))
+        if phonum:
+            break
+        else:
+            print('Mandatory field')
+
+    #get email
+
+    #insert data to database
+
+
+
+    print(dateAdd)
+
+    
+customer()
